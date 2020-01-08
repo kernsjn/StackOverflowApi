0a0cc5ba13513060b3301d2048978f29a801bac2
Justin Swiger <justin.swiger@me.com>
2020-01-08 13:41:57 -0500

    updated question controller

--- Controllers/QuestionController.cs
+++ Controllers/QuestionController.cs
@@ -9,101 +9,73 @@ using StackOverflowApi.Models;
 
 namespace StackOverflowApi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class QuestionController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class QuestionController : ControllerBase
+  {
+    private readonly DatabaseContext _context;
+
+    public QuestionController(DatabaseContext context)
     {
-        private readonly DatabaseContext _context;
-
-        public QuestionController(DatabaseContext context)
-        {
-            _context = context;
-        }
-
-        // GET: api/Question
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<QuestionPost>>> GetQuestionPosts()
-        {
-            return await _context.QuestionPosts.ToListAsync();
-        }
-
-        // GET: api/Question/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<QuestionPost>> GetQuestionPost(int id)
-        {
-            var questionPost = await _context.QuestionPosts.FindAsync(id);
-
-            if (questionPost == null)
-            {
-                return NotFound();
-            }
-
-            return questionPost;
-        }
-
-        // PUT: api/Question/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutQuestionPost(int id, QuestionPost questionPost)
-        {
-            if (id != questionPost.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(questionPost).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!QuestionPostExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Question
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPost]
-        public async Task<ActionResult<QuestionPost>> PostQuestionPost(QuestionPost questionPost)
-        {
-            _context.QuestionPosts.Add(questionPost);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetQuestionPost", new { id = questionPost.Id }, questionPost);
-        }
-
-        // DELETE: api/Question/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<QuestionPost>> DeleteQuestionPost(int id)
-        {
-            var questionPost = await _context.QuestionPosts.FindAsync(id);
-            if (questionPost == null)
-            {
-                return NotFound();
-            }
-
-            _context.QuestionPosts.Remove(questionPost);
-            await _context.SaveChangesAsync();
-
-            return questionPost;
-        }
-
-        private bool QuestionPostExists(int id)
-        {
-            return _context.QuestionPosts.Any(e => e.Id == id);
-        }
+      _context = context;
     }
+
+    // GET: api/Question
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<QuestionPost>>> GetQuestionPosts()
+    {
+      return await _context.QuestionPosts.ToListAsync();
+    }
+
+    // GET: api/Question/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<QuestionPost>> GetQuestionPost(int id)
+    {
+      var questionPost = await _context.QuestionPosts.FindAsync(id);
+
+      if (questionPost == null)
+      {
+        return NotFound();
+      }
+
+      return questionPost;
+    }
+
+    [HttpGet("AllAnswersJoin/{Id}")]
+    public ActionResult<IEnumerable<Object>> GetQuestionAnswers(int Id)
+    {
+      var answers = _context.AnswersPosts.Where(w => w.QuestionPostId == Id);
+      return answers.ToList();
+    }
+    [HttpPost("CreateQuestion")]
+    public ActionResult<QuestionPost> CreatePost([FromBody]QuestionPost entry)
+    {
+      _context.QuestionPosts.Add(entry);
+      _context.SaveChanges();
+      return entry;
+    }
+
+    [HttpPut("numberOfViews/{Id}")]
+    public ActionResult<Int32> postViews(int Id)
+    {
+      var question = _context.QuestionPosts.FirstOrDefault(f => f.Id == Id);
+      question.NumberOfViews += 1;
+      _context.SaveChanges();
+      return question.NumberOfViews;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<QuestionPost>> PostQuestionPost(QuestionPost questionPost)
+    {
+      _context.QuestionPosts.Add(questionPost);
+      await _context.SaveChangesAsync();
+
+      return CreatedAtAction("GetQuestionPost", new { id = questionPost.Id }, questionPost);
+    }
+
+    private bool QuestionPostExists(int id)
+    {
+      return _context.QuestionPosts.Any(e => e.Id == id);
+    }
+  }
 }
