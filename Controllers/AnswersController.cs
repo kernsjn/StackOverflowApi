6e3a36a9e342d3c223d1e0f0a985cfec94093b52
Justin Swiger <justin.swiger@me.com>
2020-01-08 13:28:43 -0500

    updated answers controller endpoints

--- Controllers/AnswersController.cs
+++ Controllers/AnswersController.cs
@@ -6,105 +6,58 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StackOverFlow.Models;
+using StackOverFlow.ViewModels;
 using StackOverflowApi.Models;
 
 namespace StackOverflowApi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AnswersController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnswersController : ControllerBase
+  {
+    private readonly DatabaseContext _context;
+
+    public AnswersController(DatabaseContext context)
     {
-        private readonly DatabaseContext _context;
-
-        public AnswersController(DatabaseContext context)
-        {
-            _context = context;
-        }
-
-        // GET: api/Answers
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<AnswersPost>>> GetAnswersPost()
-        {
-            return await _context.AnswersPost.ToListAsync();
-        }
-
-        // GET: api/Answers/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AnswersPost>> GetAnswersPost(int id)
-        {
-            var answersPost = await _context.AnswersPost.FindAsync(id);
-
-            if (answersPost == null)
-            {
-                return NotFound();
-            }
-
-            return answersPost;
-        }
-
-        // PUT: api/Answers/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutAnswersPost(int id, AnswersPost answersPost)
-        {
-            if (id != answersPost.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(answersPost).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!AnswersPostExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Answers
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPost]
-        public async Task<ActionResult<AnswersPost>> PostAnswersPost(AnswersPost answersPost)
-        {
-            _context.AnswersPost.Add(answersPost);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetAnswersPost", new { id = answersPost.Id }, answersPost);
-        }
-
-        // DELETE: api/Answers/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<AnswersPost>> DeleteAnswersPost(int id)
-        {
-            var answersPost = await _context.AnswersPost.FindAsync(id);
-            if (answersPost == null)
-            {
-                return NotFound();
-            }
-
-            _context.AnswersPost.Remove(answersPost);
-            await _context.SaveChangesAsync();
-
-            return answersPost;
-        }
-
-        private bool AnswersPostExists(int id)
-        {
-            return _context.AnswersPost.Any(e => e.Id == id);
-        }
+      _context = context;
     }
+
+    [HttpPost("CreateAnswer/other")]
+    public ActionResult<AnswersPost> nCreateAnswer([Bind("answersContent,questionPostId")] AnswersPost entry)
+    {
+      _context.AnswersPosts.Add(entry);
+      _context.SaveChanges();
+      return entry;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AnswersPost>>> GetAnswersPost()
+    {
+      return await _context.AnswersPost.ToListAsync();
+    }
+
+    public ActionResult<AnswersPost> CreateAnswer([FromBody]AnswersVM entry)
+    {
+      if (_context.QuestionPosts.Any(a => a.Id == entry.QuestionPostId))
+      {
+        var nAnswer = new AnswersPost
+        {
+          AnswerContent = entry.AnswerContent,
+          QuestionPostId = entry.QuestionPostId
+        };
+        _context.AnswersPosts.Add(nAnswer);
+        _context.SaveChanges();
+        return nAnswer;
+      }
+      else
+      {
+        return BadRequest();
+      }
+    }
+
+    private bool AnswersPostExists(int id)
+    {
+      return _context.AnswersPost.Any(e => e.Id == id);
+    }
+  }
 }
